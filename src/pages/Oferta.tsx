9899e24d6d7d896bb3d2a6a771a7793f912ceddd
Waynuda <waynuda12@gmail.com>
2026-04-24 12:31:18 +0100

    fix: clean up Oferta.tsx and move Pixel base to index.html

--- src/pages/Oferta.tsx
+++ src/pages/Oferta.tsx
@@ -7,39 +7,12 @@ const Oferta = () => {
   const name = location.state?.name || "Campeão";
 
   useEffect(() => {
-    // Meta Pixel Code
-    if (!(window as any).fbq) {
-      const n = ((window as any).fbq = function (...args: any[]) {
-        n.callMethod ? n.callMethod.apply(n, args) : n.queue.push(args);
-      }) as any;
-      (window as any)._fbq = n;
-      n.push = n;
-      n.loaded = true;
-      n.version = '2.0';
-      n.queue = [] as any[];
-      const t = document.createElement('script');
-      t.async = true;
-      t.src = 'https://connect.facebook.net/en_US/fbevents.js';
-      document.head.appendChild(t);
+    if (typeof (window as any).fbq === "function") {
+      (window as any).fbq("track", "PageView");
     }
-    (window as any).fbq('init', '1505287261210886');
-    (window as any).fbq('track', 'PageView');
   }, []);
 
-  return (
-    <>
-      <CheckoutPage name={name} />
-      <noscript>
-        <img
-          height="1"
-          width="1"
-          style={{ display: 'none' }}
-          src="https://www.facebook.com/tr?id=1505287261210886&ev=PageView&noscript=1"
-          alt=""
-        />
-      </noscript>
-    </>
-  );
+  return <CheckoutPage name={name} />;
 };
 
 export default Oferta;
